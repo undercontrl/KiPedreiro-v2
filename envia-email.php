1bb25c7aeb0160b64af95e7ae3efe33c13cdad65
undercontrl <lumaiza@gmail.com>
2025-09-11 11:59:22 -0300

    bd

--- envia-email.php
+++ envia-email.php
@@ -5,7 +5,7 @@ $nome = $_POST['nome'] ?? '';
 $email = $_POST['email'] ?? '';
 $senha = $_POST['senha'] ?? '';
 $ok = registrarUsuario($db, $nome, $email, $senha);
-if($ok > 0){
+if($ok > 0 || $ok === true){
     echo 'Usuário registrado com sucesso';
 }else{
     echo 'Erro ao registrar usuário';
