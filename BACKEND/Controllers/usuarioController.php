8bc97ce276208015341b61ad5a08cf16444a4237
undercontrl <lumaiza@gmail.com>
2025-09-23 10:48:07 -0300

    asdasdas

---
+++ BACKEND/Controllers/usuarioController.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Kipedreiro\Controllers;
+
+use App\Kipedreiro\Database\database;
+use App\Kipedreiro\Models\usuario;
+
+class usuarioController {
+    public $usuario;
+    public $db
+    public function __construct(){
+        $this->db = Database::getInstance()->getConnection();
+        $this->usuario = new usuario($this->db);
+    }
+    //  index
+    public function index(){
+        $resultado = $this->usuario->buscarUsuarios();
+        return $resultado;
+    }
+    //  registrar
+    
+    //  login
+
+    //  atualizar
+
+    //  deletar
+
+    //  chamada de API
+}
---
+++ BACKEND/index.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+use App\Kipedreiro\Controllers\usuarioController;
+
+$controller = new usuarioController ();
+$controller = new usuarioController();
+$resultado = $controller -> index();
+var_dump($resultado);
