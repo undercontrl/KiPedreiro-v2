c54075e55f8a5711b5fbe4706e9a7e222ae8f92b
undercontrl <lumaiza@gmail.com>
2025-09-23 09:37:22 -0300

    fdasasd

---
+++ BACKEND/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'app/kipedreiro',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '76ac47ed0e839a6dcbe73316475a29f6ad85492c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'app/kipedreiro' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '76ac47ed0e839a6dcbe73316475a29f6ad85492c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
