0a6c58857a7a9a0406138e0ad583388fcbc9d284
undercontrl <lumaiza@gmail.com>
2025-09-23 11:44:22 -0300

    piplup

--- BACKEND/index.php
+++ BACKEND/index.php
@@ -3,6 +3,15 @@ namespace App\Kipedreiro;
 require __DIR__.'/../vendor/autoload.php';
 use App\Kipedreiro\Controllers\usuarioController;
 
-$controller = new usuarioController();
-$resultado = $controller->index();
-var_dump($resultado);
+// var_dump($_SERVER["REQUEST_URI"]);
+// echo "\n\n\n\n";
+// var_dump($_SERVER["REQUEST_METHOD"]);
+// exit;
+
+if($_SERVER["REQUEST_URI"] == "/backend/buscarusuario" && $_SERVER["REQUEST_METHOD"] == "GET"){
+    $controller = new usuarioController();
+    $resultado = $controller->index();
+    var_dump($resultado);
+}else{
+    echo "Rota não encontrada";
+}
