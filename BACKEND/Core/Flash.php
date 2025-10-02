0474000eac27334b4d9e25c6c2f8296f3c9203f1
undercontrl <lumaiza@gmail.com>
2025-10-02 08:59:33 -0300

    gugudada

---
+++ BACKEND/Core/Flash.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Kipedreiro\Core;
+class Flash{
+    public static function set($type, $mensagem){
+        if(!isset($_SESSION)){
+            session_start();
+        }
+        $_SESSION["flash"] = [
+            "type" => $type,
+            "mensagem" => $mensagem,
+           
+           
+        ];
+    }
+ 
+    public static function get(){
+        if(!isset($_SESSION)){
+            session_start();
+        }
+        if(isset($_SESSION["flash"])){
+            $flash = $_SESSION["flash"];
+            unset($_SESSION["flash"]);
+            return $flash;
+        }
+        return null;
+    }
+}
