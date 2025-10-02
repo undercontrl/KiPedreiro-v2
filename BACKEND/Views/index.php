541985d762fd0dec0d367e9edb6b27d1dc6e0ddd
undercontrl <lumaiza@gmail.com>
2025-10-02 08:45:57 -0300

    sad

---
+++ BACKEND/Views/index.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Kipedreiro;
+require __DIR__.'/../vendor/autoload.php';
+use App\Kipedreiro\Rotas\Rotas;
+ 
+$rotas = Rotas::get();
+ 
+$metodoHttp = $_SERVER["REQUEST_METHOD"];
+$rota = $_SERVER["REQUEST_URI"];
+if(array_key_exists($rota, $rotas[$metodoHttp]) == false){
+    http_response_code(404);
+    echo "Página nao encontrada";
+    exit;
+}
+ 
+//              retorno string para separar em partes
+$partes = explode("@", $rotas[$metodoHttp][$rota]);
+$nomeController = $partes[0];
+$metodoController = $partes[1];
+$nomeCompletoController = "App\\Kipedreiro\\Controllers\\" . $nomeController;
+if(!class_exists($nomeCompletoController)){
+    http_response_code(500);
+    echo "O controlador não encontrado";
+    exit;
+}
+$controller = new $nomeCompletoController();
+$controller->$metodoController();
